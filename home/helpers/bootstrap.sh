3eda103904513f278cbde8c03f51dd47779ae544
Rok Pergarec <rok@parabola.si>
2015-06-01 01:09:58 +0200

    update bootstrap helper

--- home/helpers/bootstrap.sh
+++ home/helpers/bootstrap.sh
@@ -1,35 +1,37 @@
 #!/bin/sh
 
-if [ -f /etc/arch-releasea ]; then
-    sudo pacman -S tmux vim-runtime gvim-python3 python-powerline-git
+HOMESHICK_BIN=.homesick/repos/homeshick/bin/homeshick
+MR_BIN=.myrepos/mr
+
+# Do some cleanup
+rm .bash* 2> /dev/null
+
+# Install required packages
+if [[ -f /etc/arch-release && -f /usr/bin/yaourt ]]; then
+    yaourt -S tmux vim-runtime gvim-python3 python-powerline-git x11-ssh-askpass
 fi
 
 cd
 
 # Oh my zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+rm .zshrc
 
 # Homesick
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 
 # SPF13 Vim
 curl http://j.mp/spf13-vim3 -L -o - | sh
+rm .vimrc.local 2> /dev/null
 
-# Helper scripts
-git clone git@github.com:katafractari/helpers.git
-
-# MyRepos
-mkdir .myrepos
+# MyRepos and Homesick castles
+mkdir -p .myrepos
 curl -L https://raw.github.com/joeyh/myrepos/master/mr > .myrepos/mr
 chmod +x .myrepos/mr
-
-# Homesick castles
-homeshick clone git://github.com/katafractari/vcs.git
-homeshick link vcs
-
-homeshick clone git://github.com/katafractari/vim.git
-homeshick link vim
-
-homeshick clone git://github.com/katafractari/shell.git
-homeshick link shell
+curl -L https://raw.githubusercontent.com/katafractari/vcs/master/home/.mrconfig > .mrconfig
+$MR_BIN checkout
+rm .mrconfig
+$HOMESHICK_BIN link vcs
+$HOMESHICK_BIN link vim
+$HOMESHICK_BIN link shell
 
